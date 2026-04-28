1b8eac2bb6b10202de37c7729ef52f00ec7fe010
Sean Dewantoro <seanandrewvalentino@gmail.com>
2026-04-28 13:20:24 +1000

    feat: WalletContext

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { WalletProvider } from './context/WalletContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <WalletProvider>
+      <App />
+    </WalletProvider>
+  </StrictMode>
 )
