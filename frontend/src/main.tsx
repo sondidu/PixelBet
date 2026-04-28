b51ca01b0770f864b71f89c4f5e3e3139901e88e
Sean Dewantoro <seanandrewvalentino@gmail.com>
2026-04-28 13:29:23 +1000

    feat: useSimpleContract and useName

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import { WalletProvider } from './context/WalletContext.tsx'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <WalletProvider>
-      <App />
-    </WalletProvider>
+    <QueryClientProvider client={queryClient}>
+      <WalletProvider>
+        <App />
+      </WalletProvider>
+    </QueryClientProvider>
   </StrictMode>
 )
