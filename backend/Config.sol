a97a4119b79551e364de13b6ea7cde4be75a7a53
Sean Dewantoro <seanandrewvalentino@gmail.com>
2026-05-06 22:55:02 +1000

    feat(Config): constructor with constant values

--- backend/Config.sol
+++ backend/Config.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.35;
 
+uint256 constant DEFAULT_GLOBAL_MIN_BET = 100 wei;                 // 10^-16 ETH
+uint256 constant DEFAULT_GLOBAL_MAX_BET = 1000000000000000000 wei; //      1 ETH
+uint256 constant DEFAULT_HOUSE_FEE_PERCENTAGE = 5;
+uint256 constant DEFAULT_MAX_MULTIPLIER = 3000;
+uint256 constant DEFAULT_MIN_MULTIPLIER = 1000;
+
 contract Config {
     uint256 public globalMinBet;            // in wei
     uint256 public globalMaxBet;            // in wei
@@ -11,4 +17,18 @@ contract Config {
     uint256 public maxMultiplier;           // scaled by 1000, e.g. 3000 means 3x
     uint256 public minMultiplier;           // scaled by 1000, e.g. 1000 means 1x
     address public owner;                   // address of the person that deployed the contract
+
+    constructor() {
+        globalMinBet = DEFAULT_GLOBAL_MIN_BET;
+        globalMaxBet = DEFAULT_GLOBAL_MAX_BET;
+        houseFeePercentage = DEFAULT_HOUSE_FEE_PERCENTAGE;
+
+        validGridSizes = [6, 7, 8, 9, 10, 11];
+        validColourCounts = [3, 4, 5, 6, 7];
+        validBettingDurations = [300, 600, 1800, 3600];
+
+        maxMultiplier = DEFAULT_MAX_MULTIPLIER;
+        minMultiplier = DEFAULT_MIN_MULTIPLIER;
+        owner = msg.sender;
+    }
 }
