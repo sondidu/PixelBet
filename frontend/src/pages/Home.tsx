40b3c9bc2cd69ab65b576993203f9ea9ba01c89e
Sean Dewantoro <seanandrewvalentino@gmail.com>
2026-05-08 01:03:06 +1000

    feat: temporary button to navigate to /config in Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -19,6 +19,13 @@ function Home() {
       >
         Play!
       </button>
+
+      <button
+        className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-green-600 px-6 font-medium text-neutral-50 transition-all duration-75 [box-shadow:5px_5px_rgba(100,100,100,0.25)] active:translate-x-0.75 active:translate-y-0.75 active:[box-shadow:0px_0px_rgb(100_100_100)] cursor-pointer"
+        onClick={() => navigate('/config')}
+      >
+        View Config!
+      </button>
     </main>
   )
 }
