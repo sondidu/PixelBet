5aae657150848235d6afe6cd5fbc394e69c03840
Sean Dewantoro <seanandrewvalentino@gmail.com>
2026-05-07 17:17:49 +1000

    feat: integrate react-router in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import { WalletProvider } from './context/WalletContext.tsx'
+import App from './App.tsx'
 
 const queryClient = new QueryClient()
 
@@ -10,7 +11,11 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <WalletProvider>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+          </Routes>
+        </BrowserRouter>
       </WalletProvider>
     </QueryClientProvider>
   </StrictMode>
