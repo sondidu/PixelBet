cfe3b6438c7ebffd4f745302fbca2aa443481e8f
Sean Dewantoro <seanandrewvalentino@gmail.com>
2026-05-08 00:58:03 +1000

    feat: Config page and /config route
    Displays all global config state in a table with formatted
    and raw values. Requires wallet connection to read contract.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import TemporaryPage from './pages/TemporaryPage'
+import ConfigPage from './pages/Config'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/temporary-page" element={<TemporaryPage />} />
+          <Route path="/config" element={<ConfigPage />} />
         </Routes>
       </BrowserRouter>
   )
