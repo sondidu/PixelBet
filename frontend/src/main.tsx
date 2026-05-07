1e05fbdc8cb58dc573b44a8959dff379fb40f56d
Sean Dewantoro <seanandrewvalentino@gmail.com>
2026-05-07 17:24:51 +1000

    feat: TemporaryPage and move browser-router to App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter, Routes, Route } from 'react-router'
 import { WalletProvider } from './context/WalletContext.tsx'
 import App from './App.tsx'
 
@@ -11,11 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <WalletProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />} />
-          </Routes>
-        </BrowserRouter>
+        <App />
       </WalletProvider>
     </QueryClientProvider>
   </StrictMode>
