a88d25df9fd745165e1c5627ef04ec9cba48166d
xcl040 <xuchen0619@gmail.com>
2026-05-11 17:29:38 +1000

    create path for each page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import TemporaryPage from './pages/TemporaryPage'
 import ConfigPage from './pages/Config'
+import SelectPage from './pages/Select'
+import BetPage from './pages/Bet'
+import RoundHousePage from './pages/round_house'
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/select" element={<SelectPage />} />
+          <Route path='/round_house' element={<RoundHousePage />} />
+          <Route path= "/round" element={<BetPage/>} />
           <Route path="/temporary-page" element={<TemporaryPage />} />
           <Route path="/config" element={<ConfigPage />} />
         </Routes>
---
+++ frontend/src/pages/Create.tsx
@@ -0,0 +1,7 @@
+export default function RoundHousePage() {
+  return (
+    <main className="min-h-screen bg-gray-50 flex flex-col">
+        <h1 className="font-bold text-4xl text-center mt-12">Round House</h1>
+    </main>
+  )
+}
